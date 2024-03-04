1cdd052dc06192fef01e2f520e65efeed91e87ed
yujinKim <yujinkimvv@gmail.com>
2024-03-04 13:11:59 +0900

    AccountRepository 수정

--- src/main/java/com/nhnacademy/edu/springboot/AccountManagementSystem/repository/AccountRepository.java
+++ src/main/java/com/nhnacademy/edu/springboot/AccountManagementSystem/repository/AccountRepository.java
@@ -1,9 +1,10 @@
 package com.nhnacademy.edu.springboot.AccountManagementSystem.repository;
 
 import com.nhnacademy.edu.springboot.AccountManagementSystem.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AccountRepository {
-    List<Account> findAll();
+public interface AccountRepository extends JpaRepository<Account, Long> {
+//    List<Account> findAll();
 }
--- src/main/java/com/nhnacademy/edu/springboot/AccountManagementSystem/repository/DummyAccountRepository.java
+++ src/main/java/com/nhnacademy/edu/springboot/AccountManagementSystem/repository/DummyAccountRepository.java
@@ -5,10 +5,10 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
-public class DummyAccountRepository implements AccountRepository{
-    @Override
-    public List<Account> findAll() {
-        return List.of(new Account("yejun", 100000), new Account("bamby", 200000), new Account("noah", 300000));
-    }
-}
+//@Component
+//public class DummyAccountRepository implements AccountRepository{
+//    @Override
+//    public List<Account> findAll() {
+//        return List.of(new Account("yejun", 100000), new Account("bamby", 200000), new Account("noah", 300000));
+//    }
+//}
