8b592e66c338645a7106eb16dc9c4acaed24e51f
yujinKim <yujinkimvv@gmail.com>
2024-03-04 13:49:19 +0900

    AccountService 메서드 추가

--- src/main/java/com/nhnacademy/edu/springboot/AccountManagementSystem/service/AccountService.java
+++ src/main/java/com/nhnacademy/edu/springboot/AccountManagementSystem/service/AccountService.java
@@ -5,5 +5,13 @@ import com.nhnacademy.edu.springboot.AccountManagementSystem.domain.Account;
 import java.util.List;
 
 public interface AccountService {
+
     List<Account> getAccounts();
+
+    Account createAccount(Account account);
+
+    Account getAccount(Long id);
+
+    void deleteAccount(Long id);
+
 }
